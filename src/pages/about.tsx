61525d3e5ed33c6607f5cc99e593d44b11f5da4e
lesenelir <miaomiaobabyzy@gmail.com>
2023-04-14 19:53:31 +0800

    feat: file-based routing

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/about.module.css'
+
+// localhost:3000/about  --->  file-based routing
+function About() {
+  return (
+    <div className={styles.box}>
+      About
+    </div>
+  )
+}
+
+export default About
