0d79df75f831f1ccfd3ec28196c546d42f05ca32
lesenelir <miaomiaobabyzy@gmail.com>
2023-04-17 01:08:28 +0800

    feat: built-in css support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// Next.js support import css file from node_modules.
+// import 'bootstrap/dist/css/bootstrap.css'
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
