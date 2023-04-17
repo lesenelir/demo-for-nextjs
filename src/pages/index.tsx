5a7dcb4a4b2759d373ec596339996b47e3332377
lesenelir <miaomiaobabyzy@gmail.com>
2023-04-17 23:23:17 +0800

    chore: update styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from "next/link"
 
-import styles from '@/styles/home.module.css'
+import styles from '../styles/home.module.css'
 
 function Home() {
   return (
