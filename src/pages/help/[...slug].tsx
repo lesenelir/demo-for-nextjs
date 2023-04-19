2ffb873a70cd00c263e5c16d7d0b5bb718093e1a
lesenelir <miaomiaobabyzy@gmail.com>
2023-04-19 17:42:10 +0800

    chore: dynamic routing

--- src/pages/help/[...slug].tsx
+++ src/pages/help/[...slug].tsx
@@ -1,7 +1,7 @@
 /**
  * Multi-layer Dynamic Route
  *
- *  [...slug] 不捕获 /help
+ *  [...slug] 不捕获 /help （如果该文件夹内有其他的.tsx，则也不捕获该文件的内容）
  *  [[...slug]] 可以包括/help
  *
  * /help/when/what/1          {slug: ['when', 'what', '1']}
