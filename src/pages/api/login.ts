b9d5e00df4b26bd8888d9dac9f187f475483e7bf
lesenelir <miaomiaobabyzy@gmail.com>
2023-04-29 18:30:12 +0800

    feat: generate token jwt

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -3,8 +3,8 @@ import {PrismaClient} from "@prisma/client"
 import {NextApiRequest, NextApiResponse} from "next"
 import nc from 'next-connect'
 import bcrypt from "bcrypt"
-
-console.log(process.env.JWT_SECRET)
+import jwt from "jsonwebtoken"
+import {serialize} from 'cookie'
 
 const prisma = new PrismaClient()
 
@@ -29,6 +29,18 @@ const handleLogin = async (req: NextApiRequest, res: NextApiResponse) => {
     const isPasswordValid = await bcrypt.compare(password, user.password)
     if (!isPasswordValid) return res.status(401).json({message: 'Invalid password.'})
 
+    // generate jwt
+    const token: string = jwt.sign({
+      username: user.name
+    }, process.env.JWT_SECRET as string, {expiresIn: '3 days'}) // 私钥为了安全都设置为环境变量，且gitignore
+
+    const cookie: string = serialize('token', token, {
+      httpOnly: true,
+      path: '/',
+      maxAge: 60 * 60 * 24 * 3
+    })
+
+    res.setHeader('Set-Cookie', cookie)
     return res.status(200).json({message: 'Login Successful.'})
   } catch (e) {
     return res.status(500).json({message: 'Error occurred during Login.', e})
